2178cce4ce8f8642f97df73e897986449c12f266
Nick Omeyer <nick@stepsize.com>
2017-03-02 20:44:51 +0000

    Update VSCode plugin to send selected line numbers

--- vscode/README.md
+++ vscode/README.md
@@ -1,7 +1,21 @@
 ## Installing the Stepsize Layer plugin for Visual Studio Code
 
+#### The right way – terminal command
+
+`wget https://github.com/Stepsize/plugins/archive/master.zip && unzip master.zip && mv plugins-master/vscode ~/.vscode/extensions/stepsize-layer && rm -rf plugins-master master.zip`
+
+Restart Visual Studio Code after running this command and you're good to go!
+
+Note: if somehow your Visual Studio Code support directory is not located at `~/.vscode`, change the command to match your setup.
+
+#### The manual way – clicking, dragging & dropping
+
 - Download the Stepsize Layer plugin zip file [here](https://github.com/Stepsize/plugins/archive/master.zip)
 - Find it in your downloads directory and double click to unzip it – it will create a directory called `plugins-master`
-- From Visual Studio Code, select `Open` from the `File` dropdown menu
-- Find the `plugins-master` directory you just downloaded, and select the `stepsize-0.0.1.vsix` file inside the `vscode` directory and click `Open`
-- Click the `Restart` button and you're good to go!
+- Inside it that directory, there's a directory named `vscode`
+  - Copy the `vscode` directory
+  - Find your Visual Studio Code extensions directory – by default, it's in your home directory `~/.vscode/extensions`
+  - Paste the `vscode` directory inside the vscode `extensions` directory (and you can rename it to `stepsize-layer` for clarity)
+- Restart Visual Studio Code
+
+You're good to go!
--- vscode/extension.js
+++ vscode/extension.js
@@ -3,14 +3,14 @@
 var vscode = require('vscode');
 var dgram = require('dgram');
 var UDP_HOST =  "127.0.0.1", UDP_PORT = 49369;
-var pluginId = 'vscode_v0.0.1';
+var pluginId = 'vscode_v0.0.2';
 
 // this method is called when your extension is activated
 function activate(context) {
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "stepsize" is now active!');
+    console.log('Congratulations, your extension "Stepsize Layer" is now active!');
 
     var socket = dgram.createSocket("udp4"), pending = [];
 
@@ -23,22 +23,17 @@ function activate(context) {
     })
 
     vscode.window.onDidChangeTextEditorSelection(function (event) {
+      var selectedLineNumbers = getSelectedLineNumbers(event.selections);
+      var data = {
+        selectedLineNumbers,
+        filename: event.textEditor._documentData._document.fileName,
+        selected: getSelected(event.textEditor._documentData._lines, event.selections[0]),
+        plugin_id: pluginId
+      };
+      var message = JSON.stringify(data);
+      socket.send(message, 0, message.length, UDP_PORT, UDP_HOST);
       pending.push(event);
-    })
-
-    setInterval(function() {
-      if (pending.length > 0) {
-        var event = pending[pending.length - 1];
-        var data = {
-          filename: event.textEditor._documentData._document.fileName,
-          selected: getSelected(event.textEditor._documentData._lines, event.selections[0]),
-          plugin_id: pluginId
-        }
-        var message = JSON.stringify(data);
-        socket.send(message, 0, message.length, UDP_PORT, UDP_HOST);
-        pending = [];
-      }
-    }, 500);
+    });
 
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
@@ -51,6 +46,18 @@ function activate(context) {
     context.subscriptions.push(disposable);
 }
 exports.activate = activate;
+
+function getSelectedLineNumbers(selections) {
+  return selections.reduce((acc, selection) => {
+    var startLine = selection.start.line, startChar = selection.start.character;
+    var endLine = selection.end.line, endChar = selection.end.character;
+    if (startLine === endLine && startChar === endChar) return acc;
+    if (endLine > 0 && endChar === 0) endLine -= 1;
+    var numbers = [...Array(endLine - startLine + 1).keys()].map(key => key + startLine + 1);
+    acc.push(...numbers);
+    return acc;
+  }, []);
+}
 
 function getSelected (lines, selection) {
   var start = selection.start.line, end = selection.end.line;
